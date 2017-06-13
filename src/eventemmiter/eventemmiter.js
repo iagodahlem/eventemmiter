e0d00dbbd5e73b9a4381d95c78292cab751e67bf
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2017-06-13 13:41:54 -0300

    implement on

--- src/eventemmiter/eventemmiter.js
+++ src/eventemmiter/eventemmiter.js
@@ -4,7 +4,13 @@ class EventEmmiter {
   }
 
   on(event, callback) {
+    if (!this._events.hasOwnProperty(event)) {
+      this._events[event] = [callback]
+      return this
+    }
 
+    this._events[event].push(callback)
+    return this
   }
 
   emit(event) {
