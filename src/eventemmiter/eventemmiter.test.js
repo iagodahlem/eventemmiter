b38ea39614823b1fc102518639c6db7aeab6200a
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2017-06-13 13:54:23 -0300

    add emit tests

--- src/eventemmiter/eventemmiter.test.js
+++ src/eventemmiter/eventemmiter.test.js
@@ -16,18 +16,21 @@ describe('EventEmmiter', () => {
   describe('on', () => {
     it('add a event', () => {
       emmiter.on('hello', sayHello)
+
       expect(emmiter._events).toEqual({ hello: [sayHello] })
     })
 
     it('add two events', () => {
       emmiter.on('hello', sayHello)
       emmiter.on('goodbye', sayGoodbye)
+
       expect(emmiter._events).toEqual({ hello: [sayHello], goodbye: [sayGoodbye] })
     })
 
     it('add the same event twice', () => {
       emmiter.on('hello', sayHello)
       emmiter.on('hello', sayHello)
+
       expect(emmiter._events).toEqual({ hello: [sayHello, sayHello] })
     })
 
@@ -35,12 +38,39 @@ describe('EventEmmiter', () => {
       emmiter.on('hello', sayHello)
       emmiter.on('hello', sayHello)
       emmiter.on('hello', sayHello)
+
       expect(emmiter._events).toEqual({ hello: [sayHello, sayHello, sayHello] })
     })
   })
 
   describe('emit', () => {
+    it('emit non-existent event', () => {
+      expect(emmiter.emit('non-existent')).toBe(false)
+    })
 
+    it('emit an existing event', () => {
+      emmiter.on('hello', sayHello)
+
+      expect(emmiter.emit('hello')).toBe(true)
+      expect(sayHello.mock.calls.length).toBe(1)
+    })
+
+    it('emit an existing event with two equal callbacks', () => {
+      emmiter.on('hello', sayHello)
+      emmiter.on('hello', sayHello)
+
+      expect(emmiter.emit('hello')).toBe(true)
+      expect(sayHello.mock.calls.length).toBe(2)
+    })
+
+    it('emit an existing event with two diferent callbacks', () => {
+      emmiter.on('hello', sayHello)
+      emmiter.on('hello', sayGoodbye)
+
+      expect(emmiter.emit('hello')).toBe(true)
+      expect(sayHello.mock.calls.length).toBe(1)
+      expect(sayGoodbye.mock.calls.length).toBe(1)
+    })
   })
 
   describe('removeListener', () => {
